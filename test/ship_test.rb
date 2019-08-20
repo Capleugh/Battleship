d741aedd0e3dc9870192078a6784243b4c119f2a
Carleigh Crockett <capleugh@gmail.com>
2019-08-20 17:24:25 -0600

    Add Ship class and ShipTest class
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>

---
+++ test/ship_test.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_it_has_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+  end
+
+  def test_health_starts_equal_to_length
+    assert_equal 3, @cruiser.health
+  end
+
+  def test_sunk?
+    assert_equal false, @cruiser.sunk?
+  end
+
+  def test_a_hit_can_decrease_health
+    assert_equal 3, @cruiser.health
+
+    @cruiser.hit
+    assert_equal 2, @cruiser.health
+
+    @cruiser.hit
+    assert_equal 1, @cruiser.health
+  end
+end
