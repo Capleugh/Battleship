5e374921efdcb019fb9fb3287f36386215fa5c07
Rachel Lew <rlew07@gmail.com>
2019-08-23 10:33:45 -0600

    Adds separate tests for valid placement

--- test/board_test.rb
+++ test/board_test.rb
@@ -2,12 +2,13 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ship'
 require './lib/board'
-require './lib/cell'
+# require './lib/cell'
 
 class BoardTest < Minitest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
 
@@ -23,8 +24,25 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.valid_coordinate?("A22")
   end
 
-  def test_it_has_valid_placement?
+  def test_number_of_coordinates_in_array_equals_ship_length
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
     assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
+
+  def test_coordinates_are_consecutive
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
+    assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C1", "B1"])
+  end
+
+  def test_coordinates_cannot_be_diagonal
+    assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
+  end
+
+  def test_it_has_valid_placement
+    assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
+    assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 end
