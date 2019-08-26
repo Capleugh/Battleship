4acb7cea0ea0b5c875d30c8ce22e6e1fdd297b8d
Rachel Lew <rlew07@gmail.com>
2019-08-25 23:04:01 -0600

    Moves repeated variables to setup

--- test/board_test.rb
+++ test/board_test.rb
@@ -1,10 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ship'
-
-require 'pry'
 require './lib/board'
-# require './lib/cell'
 
 class BoardTest < Minitest::Test
 
@@ -12,74 +9,72 @@ class BoardTest < Minitest::Test
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
+    @coords = ["A1", "A2", "A3"]
+    @coords_2 = ["A1", "A2", "A4"]
+    @coords_3 = ["A1", "B1", "C1"]
+    @coords_4 = ["A1", "B1", "B2"]
   end
 
   def test_it_exists
-    skip
     assert_instance_of Board, @board
   end
 
   def test_it_validates_coordinates
-    skip
     assert_equal true, @board.valid_coordinate?("A1")
     assert_equal true, @board.valid_coordinate?("D4")
     assert_equal true, @board.valid_coordinate?("D4")
     assert_equal false, @board.valid_coordinate?("E1")
     assert_equal false, @board.valid_coordinate?("A22")
+
+    # @board.place(@cruiser, ["A1", "A2", "A3"])
+    #
+    # assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
   end
 
   def test_number_of_coordinates_in_array_equals_ship_length
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2"])
-    assert_equal false, @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+    assert_equal false, @board.valid_placement?(@submarine, @coords)
   end
 
   def test_all_numbers_consecutive
-    skip
-    coords = ["A1", "A2", "A3"]
-    coords_2 = ["A1", "A2", "A4"]
-    assert_equal true, @board.all_numbers_consecutive?(coords)
-    assert_equal false, @board.all_numbers_consecutive?(coords_2)
+    assert_equal true, @board.all_numbers_consecutive?(@coords)
+    assert_equal false, @board.all_numbers_consecutive?(@coords_2)
   end
 
   def test_all_numbers_same
-    skip
-    coords_3 = ["A1", "B1", "C1"]
-    coords_4 = ["A1", "B1", "B2"]
-    assert_equal true, @board.all_numbers_same?(coords_3)
-    assert_equal false, @board.all_numbers_same?(coords_4)
+    assert_equal true, @board.all_numbers_same?(@coords_3)
+    assert_equal false, @board.all_numbers_same?(@coords)
   end
 
   def test_all_letters_consecutive
-    skip
-    coords_3 = ["A1", "B1", "C1"]
-    coords_4 = ["A1", "B1", "B2"]
-    assert_equal true, @board.all_letters_consecutive?(coords_3)
-    assert_equal false, @board.all_letters_consecutive?(coords_4)
+    assert_equal true, @board.all_letters_consecutive?(@coords_3)
+    assert_equal false, @board.all_letters_consecutive?(@coords_4)
   end
 
   def test_all_letters_same
-    skip
-    coords = ["A1", "A2", "A3"]
-    coords_4 = ["A1", "B1", "B2"]
-    assert_equal true, @board.all_letters_same?(coords)
-    assert_equal false, @board.all_letters_same?(coords_4)
+    assert_equal true, @board.all_letters_same?(@coords)
+    assert_equal false, @board.all_letters_same?(@coords_4)
   end
 
   def test_valid_row
-    skip
-    assert_equal true, @board.valid_row?(@cruiser, ["A1", "A2", "A3"])
+    assert_equal true, @board.valid_row?(@coords)
+    assert_equal false, @board.valid_row?(@coords_4)
   end
 
   def test_valid_column
-    coords_3 = ["A1", "B1", "C1"]
-    coords_4 = ["A1", "B1", "B2"]
-    assert_equal true, @board.valid_column?(coords_3)
-    assert_equal false, @board.valid_column?(coords_4)
+    assert_equal true, @board.valid_column?(@coords_3)
+    assert_equal false, @board.valid_column?(@coords_4)
   end
 
+  def test_ships_do_not_overlap
+    skip
+    @board.place(@cruiser, @coords)
+
+    assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
+    # part of valid_placement? method
+  end
 
   def test_coordinates_are_consecutive
-    skip
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
     assert_equal false, @board.valid_placement?(@submarine, ["A1", "C1"])
     assert_equal false, @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
@@ -87,13 +82,11 @@ class BoardTest < Minitest::Test
   end
 
   def test_coordinates_cannot_be_diagonal
-    skip
     assert_equal false, @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
     assert_equal false, @board.valid_placement?(@submarine, ["C2", "D3"])
   end
 
   def test_it_has_valid_placement
-    skip
     assert_equal true, @board.valid_placement?(@submarine, ["A1", "A2"])
     assert_equal true, @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
@@ -110,12 +103,5 @@ class BoardTest < Minitest::Test
     assert_equal @cruiser, cell_2.ship
     assert_equal @cruiser, cell_3.ship
     assert_equal true, cell_3.ship == cell_2.ship
-  end
-
-  def test_ships_do_not_overlap
-    skip
-    @board.place(@cruiser, ["A1", "A2", "A3"])
-
-    assert_equal false, @board.valid_placement?(@submarine, ["A1", "B1"])
   end
 end
