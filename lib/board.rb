1b3661a09ad81c69d266bcb13f7707e7bf3b516b
Rachel Lew <rlew07@gmail.com>
2019-08-27 11:25:37 -0600

    Adds place method
    Co-authored-by: Carleigh Crockett <47065120+capleugh@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -78,4 +78,12 @@ class Board
       false
     end
   end
+
+  def place(ship, coordinates)
+    if valid_placement?(ship, coordinates)
+      coordinates.each do |coordinate|
+      @cells[coordinate].place_ship(ship)
+      end
+    end
+  end
 end
