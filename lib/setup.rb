c1c451515b48b0e4d3b3128e1583d5d3710d2d87
Rachel Lew <rlew07@gmail.com>
2019-08-28 17:42:45 -0600

    Adds methods to initiate game, generate random coordinates, place ships, display winner, end game

--- lib/setup.rb
+++ lib/setup.rb
@@ -21,17 +21,32 @@ class Setup
     end
   end
 
-  def initiate_game
+  def initiate_game(board)
     # put place computer ships and place player ships in this method
+    place_computer_ships
+    place_players_ships
+    display_winner
+    main_menu
+  end
+
+  def generate_random_coordinates
+    # range of coordinates to array, sample (3)
+    coordinates = board.cells.keys
+    coordinates.to_a.sample(3)
   end
 
   def place_computer_ships(board)
     # generate random coordinates?
-    if coordinates.valid_placement?
-      @board.place(ship, coordinates)
-      # when ships are placed at valid positions, trigger player ship placement
-    else
-      # generate random coordinates again until valid
+    until coordinates.valid_placement?
+      generate_random_coordinates
+      if coordinates.valid_placement?
+        @board.place(ship, coordinates)
+        # when ships are placed at valid positions
+      else
+        # generate random coordinates again until valid
+        generate_random_coordinates
+        coordinates
+      end
     end
   end
 
@@ -43,12 +58,26 @@ class Setup
     puts "Enter the squares for the Cruiser (3 spaces):
     >"
     input = gets.chomp
-    if input.valid_placement?
-      @board.place(ship, coordinates)
-      # loop back up to input again to ask a second time
-    else
-      puts "Enter valid coordinates for your ships. Ships can be placed horizontally or diagonally in empty spaces but may not be placed diagonally."
-      # loop back up to input
+    until input.valid_placement?
+      if input.valid_placement?
+        @board.place(ship, coordinates)
+        # loop back up to input again to ask a second time
+
+      else
+        puts "Enter valid coordinates for your ships. Ships can be placed horizontally or diagonally in empty spaces but may not be placed diagonally."
+        input = gets.chomp
+      end
     end
   end
+
+  def display_winner(winner)
+    @winner = winner
+  end
+
+  def end_game
+
+  end
 end
+
+# research how to generate random stuff (coordinats, hit coordinates, look back at valid_placement? method to maek sure cells are lined up properly)
+# look up range function
